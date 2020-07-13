4ac45608cd3e243e037b096edda3ae72ca9b921a
dheemanthrk <dheemanthkumawat@gmail.com>
2020-07-13 20:07:14 +0530

    Added Comments to Same tree

--- Week_2/Same_tree.py
+++ Week_2/Same_tree.py
@@ -1,3 +1,9 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isSameTree(self, p, q):
         if p and not q or q and not p: return False
